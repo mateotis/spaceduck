8392a22b37a9b3dec6fdc8848bdfe2a3cdbced22
mateotis <hekfuszmate@gmail.com>
2022-03-02 17:22:41 +0400

    Started working on the dialogue system.

--- spaceduck.js
+++ spaceduck.js
@@ -1,3 +1,20 @@
+let nextDiagReady = false;
+
 $(document).ready(function() {
-    $("#duck").animate({left: "+=85%"}, 10000, "linear");
+    $("#duck").animate({left: "40%"}, 10000, "linear", function() {
+		$('#panel2-diag1').fadeIn();
+		nextDiagReady = true; // Enable advancing the dialogue only after the first line has appeared
+		$("#duck").animate({left: "85%"}, 11250, "linear"); // These numbers ensure constant speed in both stages (if one changes, the other should too!)
+	});
+
+});
+
+$(document).ready(function() {
+	$(document).click(function() {
+		if(nextDiagReady == true) {
+			$("#panel2-diag1").fadeOut(function() {
+				$("#panel2-diag2").fadeIn();
+			});
+		}
+	});
 });
