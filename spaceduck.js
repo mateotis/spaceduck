d5f748c6f1722590ef258b047249cd20bede9334
mateotis <hekfuszmate@gmail.com>
2022-03-03 11:22:45 +0400

    Started on panel 3 with the duck following the cursor.

--- spaceduck.js
+++ spaceduck.js
@@ -1,10 +1,12 @@
 let nextDiagReady = false;
+let finishedPanel = 0;
+let currentPanel = 2;
 
 $(document).ready(function() {
-    $("#duck").animate({left: "40%"}, 10000, "linear", function() {
+    $("#duck2").animate({left: "40%"}, 10000, "linear", function() {
 		$('#panel2-diag1').fadeIn();
 		nextDiagReady = true; // Enable advancing the dialogue only after the first line has appeared
-		$("#duck").animate({left: "85%"}, 11250, "linear"); // These numbers ensure constant speed in both stages (if one changes, the other should too!)
+		$("#duck2").animate({left: "85%"}, 11250, "linear"); // These numbers ensure constant speed in both stages (if one changes, the other should too!)
 	});
 
 });
@@ -14,7 +16,27 @@ $(document).ready(function() {
 		if(nextDiagReady == true) {
 			$("#panel2-diag1").fadeOut(function() {
 				$("#panel2-diag2").fadeIn();
+				finishedPanel = 2;
 			});
 		}
+	});
+});
+
+$(document).ready(function() {
+	$(document).click(function() {
+		if(finishedPanel == 2 && currentPanel == 2) {
+			$("#duck2").remove();
+			$("#panel3").append("<img class = 'duck' id = 'duck3' src = 'duck.png' width = '200' height = '100'>")
+			currentPanel = 3;
+		}
+	});
+});
+
+$(document).ready(function() {
+	$(document).on('mousemove', (event) => {
+		$('#duck3').css({
+			left: event.clientX,
+			top: event.clientY,
+		});
 	});
 });
